10f05e80d886b8d1b1d105cb7e45e211577dea75
Avijit Mondal <avijitmondal38@gmail.com>
2017-09-20 15:24:16 +0530

    Changed URIContant class to URIConstant interface

--- src/main/java/com/avijit/together/server/util/URIConstant.java
+++ src/main/java/com/avijit/together/server/util/URIConstant.java
@@ -7,21 +7,21 @@ package com.avijit.together.server.util;
  * @author avijit
  *
  */
-public class URIConstant {
+public interface URIConstant {
 	/* Rest API URI */
-	public static final String API_BASE_URI = "/api";
-	public static final String API_USERS_URI = API_BASE_URI + "/users";
-	public static final String API_AUTHENTICATE_URI = API_BASE_URI + "/authentications";
-	public static final String API_AUTHENTICATE_LOGOUT_URI = API_AUTHENTICATE_URI + "/logout";
-	public static final String API_CONVERSATIONS_URI = API_BASE_URI + "/conversations";
+	String API_BASE_URI = "/api";
+	String API_USERS_URI = API_BASE_URI + "/users";
+	String API_AUTHENTICATE_URI = API_BASE_URI + "/authentications";
+	String API_AUTHENTICATE_LOGOUT_URI = API_AUTHENTICATE_URI + "/logout";
+	String API_CONVERSATIONS_URI = API_BASE_URI + "/conversations";
 
 	/* Chat URI */
-	public static final String URI_BROKER_ENDPOINT = "/together/websocket";
-	public static final String URI_DESTINATION_PREFIX = "/app";
-	public static final String URI_BROKER_TOPIC = "/topic";
-	public static final String URI_BROKER_QUEUE = "/queue";
-	public static final String URI_GROUP_CHAT = "/groupchats";
-	public static final String URI_PERSONAL_CHAT = "/personalchats";
-	public static final String URI_TOPIC_GROUP_CHAT_RESPONSE = "/topic/groupchats.";
-	public static final String URI_QUEUE_PERSONAL_CHAT_RESPONSE = "/queue/personalchats.";
+	String URI_BROKER_ENDPOINT = "/together/websocket";
+	String URI_DESTINATION_PREFIX = "/app";
+	String URI_BROKER_TOPIC = "/topic";
+	String URI_BROKER_QUEUE = "/queue";
+	String URI_GROUP_CHAT = "/groupchats";
+	String URI_PERSONAL_CHAT = "/personalchats";
+	String URI_TOPIC_GROUP_CHAT_RESPONSE = "/topic/groupchats.";
+	String URI_QUEUE_PERSONAL_CHAT_RESPONSE = "/queue/personalchats.";
 }
