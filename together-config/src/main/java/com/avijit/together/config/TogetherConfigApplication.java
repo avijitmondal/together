bbc26a190f3e536c24f1104debf87ff4b3b18152
Avijit Mondal <avijitmondal38@gmail.com>
2019-08-06 19:57:16 +0530

    Added config module
    Former-commit-id: 290ffb5a292be6ad4cb13fda7288e5f05d04e847

---
+++ together-config/src/main/java/com/avijit/together/config/TogetherConfigApplication.java
@@ -0,0 +1,13 @@
+package com.avijit.together.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetherConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogetherConfigApplication.class, args);
+	}
+
+}
