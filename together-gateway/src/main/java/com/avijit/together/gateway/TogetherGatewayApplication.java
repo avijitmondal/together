af9edc7b87990ab0a9d2d45f2acdc22ff3d74768
Avijit Mondal <avijitmondal38@gmail.com>
2019-08-20 21:12:44 +0530

    Added admin client dependency to all the modules
    Former-commit-id: 162123d5b774fce5b7a2003d1ca09750e50e6872

--- together-gateway/src/main/java/com/avijit/together/gateway/TogetherGatewayApplication.java
+++ together-gateway/src/main/java/com/avijit/together/gateway/TogetherGatewayApplication.java
@@ -6,10 +6,12 @@ import com.avijit.together.gateway.filter.PreFilter;
 import com.avijit.together.gateway.filter.RouteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableZuulProxy
 public class TogetherGatewayApplication {
 
