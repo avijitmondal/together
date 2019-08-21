19321005d72d1a288bf42c9465051981b89da36d
Avijit Mondal <avijitmondal38@gmail.com>
2019-08-21 18:45:30 +0530

    COnverted I_Constant from interface to class
    Former-commit-id: 627ebe39c4f335ddf989fae9d0bc68445faa17da

--- together-core/src/main/java/com/avijit/together/core/data/I_Constant.java
+++ together-core/src/main/java/com/avijit/together/core/data/I_Constant.java
@@ -7,81 +7,97 @@ package com.avijit.together.core.data;
  * @author avijit
  *
  */
-public interface I_Constant {
+public abstract class I_Constant {
 
 	/* Rest API URI */
 	/**
 	 * 
 	 */
-	String API_BASE = "/api";
-	String API_VERSION = "/v1";
-	String API_PREFIX = API_BASE + API_VERSION;
+	public static final String API_BASE = "/api";
+	public static final String API_VERSION = "/v1";
+	public static final String API_PREFIX = API_BASE + API_VERSION;
 	/**
 	 * 
 	 */
-	String API_USERS = API_PREFIX + "/users";
+	public static final String API_USERS = API_PREFIX + "/users";
 	/**
 	 * 
 	 */
-	String API_AUTHENTICATIONS = API_PREFIX + "/authentications";
+	public static final String API_AUTHENTICATIONS = API_PREFIX + "/authentications";
 	/**
 	 * 
 	 */
-	String API_CONVERSATIONS = API_PREFIX + "/conversations";
+	public static final String API_CONVERSATIONS = API_PREFIX + "/conversations";
 	/*
 	*
 	*
 	*/
-	String API_PROFILE_PICTURES = API_PREFIX + "/profile_pictures";
+	public static final String API_PROFILE_PICTURES = API_PREFIX + "/profile_pictures";
 	/**
 	 * 
 	 */
-	String API_CONVERSATION_PARTICIPANTS = API_CONVERSATIONS + "/{conversation_id}/participants";
+	public static final String API_CONVERSATION_PARTICIPANTS = API_CONVERSATIONS + "/{conversation_id}/participants";
 
-	String API_MESSAGES = API_CONVERSATIONS + "/{conversation_id}/messages";
+	public static final String API_MESSAGES = API_CONVERSATIONS + "/{conversation_id}/messages";
 
-	String API_CONVERSATION_FILES = API_CONVERSATIONS + "/ftp/{conversation_id}/files";
+	public static final String API_CONVERSATION_FILES = API_CONVERSATIONS + "/ftp/{conversation_id}/files";
 
 	/* Chat URI */
 	/**
 	 * 
 	 */
-	String URI_BROKER_ENDPOINT = "/together/websocket";
+	public static final String URI_BROKER_ENDPOINT = "/together/websocket";
 	/**
 	 * 
 	 */
-	String URI_DESTINATION_PREFIX = "/app";
+	public static final String URI_DESTINATION_PREFIX = "/app";
 	/**
 	 * 
 	 */
-	String URI_BROKER_TOPIC = "/topic";
+	public static final String URI_BROKER_TOPIC = "/topic";
 	/**
 	 * 
 	 */
-	String URI_BROKER_QUEUE = "/queue";
+	public static final String URI_BROKER_QUEUE = "/queue";
 	/**
 	 * 
 	 */
-	String URI_GROUP_CHAT = "/groupchats";
+	public static final String URI_GROUP_CHAT = "/groupchats";
 	/**
 	 * 
 	 */
-	String URI_PERSONAL_CHAT = "/personalchats";
+	public static final String URI_PERSONAL_CHAT = "/personalchats";
 	/**
 	 * 
 	 */
-	String URI_TOPIC_GROUP_CHAT_RESPONSE = "/topic/groupchats.";
+	public static final String URI_TOPIC_GROUP_CHAT_RESPONSE = "/topic/groupchats.";
 	/**
 	 * 
 	 */
-	String URI_QUEUE_PERSONAL_CHAT_RESPONSE = "/queue/personalchats.";
+	public static final String URI_QUEUE_PERSONAL_CHAT_RESPONSE = "/queue/personalchats.";
 
-	String FILES_LOCATION = "C://together//db//files//";
+	public static final String FILES_LOCATION = "C://together//db//files//";
 
-	String TEMPORARY_FILES_LOCATION = "C://together//db//files//temporary//";
+	public static final String TEMPORARY_FILES_LOCATION = "C://together//db//files//temporary//";
 
-	String FILE_URI = "/%s/files?file_name=%s&s_id=%s";
+	public static final String FILE_URI = "/%s/files?file_name=%s&s_id=%s";
 
-	String AUTH_TOKEN = "AUTH_TOKEN";
-	String AUTHORIZATION = "Authorization";
+	public static final String AUTH_TOKEN = "AUTH_TOKEN";
+	public static final String AUTHORIZATION = "Authorization";
+
+	// Rest URL
+	public static final String URI_HTTP = "http://";
+	public static final String URI_HTTPS = "https://";
+
+	// Service names
+	public static final String SERVICE_TOGETHER_CONFIG = "together-config";
+	public static final String SERVICE_TOGETHER_CONVERSATION = "together-conversation";
+	public static final String SERVICE_TOGETHER_DATABASE = "together-databse";
+	public static final String SERVICE_TOGETHER_FTP = "together-ftp";
+	public static final String SERVICE_TOGETHER_GATEWAY = "together-gateway";
+	public static final String SERVICE_TOGETHER_LOGIN = "together-login";
+	public static final String SERVICE_TOGETHER_REGISTRATION = "together-registration";
+	public static final String SERVICE_TOGETHER_STATUS = "together-status";
+	public static final String SERVICE_TOGETHER_USER = "together-user";
+
 }
