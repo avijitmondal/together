1d3e503938958d74299c39b8769a290472e33588
Avijit Mondal <avijitmondal38@gmail.com>
2019-09-11 23:48:06 +0530

    Interface to class error details
    Former-commit-id: 85bde296f997aac37609b28babe9723e40303b8b

--- together-core/src/main/java/com/avijit/together/core/exception/IErrorDetails.java
+++ together-core/src/main/java/com/avijit/together/core/exception/IErrorDetails.java
@@ -11,43 +11,43 @@ package com.avijit.together.core.exception;
  * @author avijit
  *
  */
-public interface IErrorDetails {
-	String USER_ID_NOT_FOUND = "User with ID %s not found";
-	String MESSAGE_ID_NOT_FOUND = "Message with ID %s not found";
-	String AUTHENTICATION_ID_NOT_FOUND = "Authentication with ID %s not found";
-	String CONVERSATION_ID_NOT_FOUND = "Conversation with ID %s not found";
-	String USER_EMAIL_NOT_FOUND = "User with email ID %s not found";
-	String USER_USERNAME_NOT_FOUND = "User with username %s not found";
-	String CONVERSATIONS_USER_ID_NOT_FOUND = "Conversation with user ID %s not found";
-	String FILE_NOT_FOUND = "File with name %s not found";
+public abstract class IErrorDetails {
+	public static final String USER_ID_NOT_FOUND = "User with ID %s not found";
+	public static final String MESSAGE_ID_NOT_FOUND = "Message with ID %s not found";
+	public static final String AUTHENTICATION_ID_NOT_FOUND = "Authentication with ID %s not found";
+	public static final String CONVERSATION_ID_NOT_FOUND = "Conversation with ID %s not found";
+	public static final String USER_EMAIL_NOT_FOUND = "User with email ID %s not found";
+	public static final String USER_USERNAME_NOT_FOUND = "User with username %s not found";
+	public static final String CONVERSATIONS_USER_ID_NOT_FOUND = "Conversation with user ID %s not found";
+	public static final String FILE_NOT_FOUND = "File with name %s not found";
 	
-	String UNABLE_TO_ADD_USER = "Unable to add new user";
-	String UNABLE_TO_ADD_CONVERSATION = "Unable to add new conversation";
-	String UNABLE_TO_ADD_AUTHENTICATION = "Unable to add new authentication";
-	String UNABLE_TO_ADD_MESSAGE = "Unable to add new message";
-	String UNABLE_TO_ADD_PARTICIPANT = "Unable to add new participant";
-	String UNABLE_TO_ADD_FILE = "Unable to add new file";
+	public static final String UNABLE_TO_ADD_USER = "Unable to add new user";
+	public static final String UNABLE_TO_ADD_CONVERSATION = "Unable to add new conversation";
+	public static final String UNABLE_TO_ADD_AUTHENTICATION = "Unable to add new authentication";
+	public static final String UNABLE_TO_ADD_MESSAGE = "Unable to add new message";
+	public static final String UNABLE_TO_ADD_PARTICIPANT = "Unable to add new participant";
+	public static final String UNABLE_TO_ADD_FILE = "Unable to add new file";
 	
-	String INVALID_USER_ID = "Invalid user ID: %s";
-	String INVALID_MESSAGE_ID = "Invalid message ID: %s";
-	String INVALID_AUTHENTICATION_ID = "Invalid authentication ID: %s";
-	String INVALID_CONVERSATION_ID = "Invalid conversation ID: %s";
-	String INVALID_PARTICIPANT_ID = "Invalid participant ID: %s";
-	String INVALID_FILE_ATTACHED = "Invalid file attached: %s";
+	public static final String INVALID_USER_ID = "Invalid user ID: %s";
+	public static final String INVALID_MESSAGE_ID = "Invalid message ID: %s";
+	public static final String INVALID_AUTHENTICATION_ID = "Invalid authentication ID: %s";
+	public static final String INVALID_CONVERSATION_ID = "Invalid conversation ID: %s";
+	public static final String INVALID_PARTICIPANT_ID = "Invalid participant ID: %s";
+	public static final String INVALID_FILE_ATTACHED = "Invalid file attached: %s";
 	
-	String ENTER_VALID_USER_ID = "Please enter a valid user ID";
-	String ENTER_VALID_USER_EMAIL = "Please enter a valid email ID";
-	String ENTER_VALID_CONVERSATION_ID = "Please enter a valid conversation ID";
-	String ENTER_VALID_PARTICIPANT_ID = "Please enter a valid participant ID";
-	String ENTER_VALID_MESSAGE_ID = "Please enter a valid message ID";
-	String ENTER_VALID_AUTHENTICATION_ID = "Please enter a valid authentication ID";
-	String ENTER_VALID_FILE_NAME = "Please enter a valid file name";
-	String ENTER_VALID_FILE = "Please attach a valid file";
-	String ENTER_VALID_DATA = "Please enter valid data(s)";
+	public static final String ENTER_VALID_USER_ID = "Please enter a valid user ID";
+	public static final String ENTER_VALID_USER_EMAIL = "Please enter a valid email ID";
+	public static final String ENTER_VALID_CONVERSATION_ID = "Please enter a valid conversation ID";
+	public static final String ENTER_VALID_PARTICIPANT_ID = "Please enter a valid participant ID";
+	public static final String ENTER_VALID_MESSAGE_ID = "Please enter a valid message ID";
+	public static final String ENTER_VALID_AUTHENTICATION_ID = "Please enter a valid authentication ID";
+	public static final String ENTER_VALID_FILE_NAME = "Please enter a valid file name";
+	public static final String ENTER_VALID_FILE = "Please attach a valid file";
+	public static final String ENTER_VALID_DATA = "Please enter valid data(s)";
 
-	String TRY_SOMETIME_LATER = "Please try after some time";
+	public static final String TRY_SOMETIME_LATER = "Please try after some time";
 
-	String UNABLE_TO_CREATE_DIRECTORY = "Unable to create directory";
-	String FILE_NOT_ADDED = "File with name %s not added";
-	String INVALID_FILE_NAME = "Invalid filename %s";
+	public static final String UNABLE_TO_CREATE_DIRECTORY = "Unable to create directory";
+	public static final String FILE_NOT_ADDED = "File with name %s not added";
+	public static final String INVALID_FILE_NAME = "Invalid filename %s";
 }
