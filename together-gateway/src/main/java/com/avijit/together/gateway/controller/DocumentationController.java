ae463a307b4d9e6c57aaac11698745fe201b158d
Avijit Mondal <avijitmondal38@gmail.com>
2019-09-17 22:32:23 +0530

    Integrated swagger-ui with together-conversation and together-user
    Former-commit-id: d2f4adfe5025fcda4d63cd92467d45bfddcbc8d7

---
+++ together-gateway/src/main/java/com/avijit/together/gateway/controller/DocumentationController.java
@@ -0,0 +1,34 @@
+package com.avijit.together.gateway.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+@Component
+@Primary
+@EnableAutoConfiguration
+public class DocumentationController implements SwaggerResourcesProvider {
+
+	@Override
+	public List<SwaggerResource> get() {
+		List<SwaggerResource> resources = new ArrayList<>();
+		resources.add(swaggerResource("together-conversation", "/api/v1/conversations/v2/api-docs", "2.0"));
+		resources.add(swaggerResource("together-user", "/api/v1/users/v2/api-docs", "2.0"));
+		return resources;
+	}
+
+	private SwaggerResource swaggerResource(String name, String location, String version) {
+		SwaggerResource swaggerResource = new SwaggerResource();
+		swaggerResource.setName(name);
+		swaggerResource.setLocation(location);
+		swaggerResource.setSwaggerVersion(version);
+		return swaggerResource;
+	}
+
+}
--- together-user/src/main/java/com/avijit/together/user/TogetherUserApplication.java
+++ together-user/src/main/java/com/avijit/together/user/TogetherUserApplication.java
@@ -1,15 +1,46 @@
 package com.avijit.together.user;
 
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.xmlpull.v1.XmlPullParserException;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.io.FileReader;
+import java.io.IOException;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableSwagger2
 public class TogetherUserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TogetherUserApplication.class, args);
 	}
 
+	@Bean
+	public Docket api() {
+		MavenXpp3Reader reader = new MavenXpp3Reader();
+		Model model = null;
+		try {
+			model = reader.read(new FileReader("pom.xml"));
+		} catch (Exception exception) {
+
+		}
+
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.avijit.together.user.controller"))
+				.paths(PathSelectors.any())
+				.build().apiInfo(new ApiInfo("Account Service Api Documentation", "Documentation automatically generated", model.getParent().getVersion(), null, new Contact("Avijit Mondal", "avijitmondal.wordpress.com", "avijitmondal38@gmail.com"), null, null));
+	}
 }
