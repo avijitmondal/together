73a402f78347afd25d62c51f914cbd846f44ed93
Avijit Mondal <avijitmondal38@gmail.com>
2020-02-25 16:28:35 +0530

    Modified together status #12
    Split eureka and SBA, now contains only spring boot admin

--- together-status/src/main/java/com/avijit/together/status/TogetherStatusApplication.java
+++ together-status/src/main/java/com/avijit/together/status/TogetherStatusApplication.java
@@ -3,15 +3,16 @@ package com.avijit.together.status;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableDiscoveryClient
 @EnableAdminServer
+@EnableScheduling
 public class TogetherStatusApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TogetherStatusApplication.class, args);
 	}
-
 }
