f01b41ebd8c95075c303773e56f8cfa35b6ee68e
Avijit Mondal <avijitmondal38@gmail.com>
2020-02-25 16:37:39 +0530

    Added together-discovery microservice to discover eureka clients #15

---
+++ together-discovery/src/test/java/io/github/avijitmondal/together/discovery/TogetherDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.avijitmondal.together.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogetherDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
