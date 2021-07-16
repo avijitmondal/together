0cf7c68d711e8821c35fd5346a03d0a5d2bdfed3
Avijit <avijitmondal38@gmail.com>
2021-07-17 00:19:12 +0530

    Added dependency for h2 and jpa n together-user

---
+++ together-user/src/main/java/com/avijitmondal/together/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.avijitmondal.together.user.repository;
+
+import com.avijitmondal.together.core.dto.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+//@Repository
+//public interface UserRepository extends JpaRepository<User, UUID> {
+//}
