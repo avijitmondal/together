7a2a75e542ebeadd99f73e9bd1f7ac9dc57ac954
Avijit <avijitmondal38@gmail.com>
2021-07-27 12:46:34 +0530

    Added together-user testcase

--- together-user/src/test/java/com/avijitmondal/together/user/UserApplicationTests.java
+++ together-user/src/test/java/com/avijitmondal/together/user/UserApplicationTests.java
@@ -1,7 +1,10 @@
 package com.avijitmondal.together.user;
 
+import com.avijitmondal.together.user.controller.UserController;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserApplicationTests {
 
+	@Autowired
+	private UserController userController;
+
 	@Test
 	public void contextLoads() {
+		Assertions.assertNotNull(userController);
 	}
 
 }
---
+++ together-user/src/test/java/com/avijitmondal/together/user/controller/UserControllerTest.java
@@ -0,0 +1,24 @@
+package com.avijitmondal.together.user.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void findAll() throws Exception {
+        mockMvc.perform(get("/api/v1/users"))
+                .andExpect(status().isOk());
+    }
+}
