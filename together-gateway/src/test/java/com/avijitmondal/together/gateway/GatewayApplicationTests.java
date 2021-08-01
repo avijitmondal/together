db036e00f86eb5a7eee30347baf3f17022f7bedf
Avjit Mondal <avijitmondal38@gmail.com>
2021-08-01 10:09:45 +0530

    Increased coverage together-gateway

--- together-gateway/src/test/java/com/avijitmondal/together/gateway/GatewayApplicationTests.java
+++ together-gateway/src/test/java/com/avijitmondal/together/gateway/GatewayApplicationTests.java
@@ -5,12 +5,12 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GatewayApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void main() {
+		GatewayApplication.main(new String[]{});
 	}
 
 }
