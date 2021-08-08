4083200959d6d88b6e9b0639b18c33e296cd8b2e
Avijit <avijitmondal38@gmail.com>
2021-08-08 20:50:28 +0530

    Added test coverage for alive

--- together-alive/src/test/java/com/avijitmondal/together/alive/AliveApplicationTests.java
+++ together-alive/src/test/java/com/avijitmondal/together/alive/AliveApplicationTests.java
@@ -1,18 +1,16 @@
 package com.avijitmondal.together.alive;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class AliveApplicationTests {
+class AliveApplicationTests {
 
 
     @Test
-    public void contextLoads() {
-
+    void main() {
+        AliveApplication.main(new String[]{});
+        Assertions.assertTrue(true);
     }
-
 }
